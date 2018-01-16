c964f1c83f89ac90f2c86751ab6aabb25d204c3d
Jeremy Osterhouse <josterhouse@gmail.com>
2018-01-15 22:06:55 -0500

    Add outside temp and thermostat set functions

--- temp/temp.py
+++ temp/temp.py
@@ -1,6 +1,7 @@
 
 import os
 import glob
+from datetime import datetime
 import time
 import sqlite3
  
@@ -10,9 +11,52 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
 base_dir = '/sys/bus/w1/devices/'
-dbname = "/home/pi/templog.db"
+dbname = "/home/josterpi/raspberrypi/templog.db"
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+def weekend(dt):
+    if dt.weekday() in (5, 6):
+        return True
+    return False
+
+def datetime_compare(a, b):
+    if isinstance(a, datetime):
+        a = (a.hour, a.minute)
+    if isinstance(b, datetime):
+        b = (b.hour, b.minute)
+    if cmp(a[0], b[0]) == 0:
+        return cmp(a[1], b[1])
+    else:
+        return cmp(a[0, b[0])
+
+def gte(a, b):
+    return datetime_compare(a, b) in (0, 1)
+
+def lt(a, b):
+    return datetime_compare(a, b) == -1
+
+# Schedule
+# MF 5:45am 70
+#    9:00am 68
+#   10:00pm 65
+# SS 6:00am 70
+#    8:00am 68
+#    9:00pm 65
+
+def schedule_temp(dt):
+    schedule = {"MF": [(0, 0, 65), (5, 45, 70), (9, 0, 68), (22, 0, 65)],
+                "SS": [(0, 0, 65), (6, 0, 70), (8, 0, 68), (21, 0, 65)]}
+    group = "MF"
+    if weekend(dt):
+        group = "SS"
+    temp = 0
+    for point in schedule[group]:
+        if gte(dt, point):
+            temp = point[2]
+        if lt(dt, point):
+            break
+    return temp
  
 def read_temp_raw():
     f = open(device_file, 'r')
---
+++ temp/weather.py
@@ -0,0 +1,15 @@
+
+import json
+import urllib.request
+
+from secrets import WEATHER_KEY, CITY_ID
+
+API = "http://api.openweathermap.org/data/2.5/weather?units=imperial&id=%s&appid=%s"
+
+def get_url():
+    return API % (CITY_ID, WEATHER_KEY)
+
+def get_weather():
+    res = urllib.request.urlopen(get_url())
+    weather = json.loads(res.read().decode('utf-8'))
+    return weather
